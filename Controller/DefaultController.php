ccdc6285423af1fe79746aa588ef761015ce6aee
looky_alba <looky.alba@gmail.com>
2016-07-20 16:42:49 +0300

    restAPI from symfony3

---
+++ Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace RestAPIBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("restapi/")
+     */
+    public function indexAction()
+    {
+        return $this->render('RestAPIBundle:Default:index.html.twig');
+    }
+}
